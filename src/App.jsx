63b0d1cb0c8cc05ef8380dc114c932a642c177c0
saju1121 <sakilahmedsaju1121@gmail.com>
2022-08-14 21:15:24 +0600

    Register and Login page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 // import Home from "./Pages/Home";
 
-import Product from "./Pages/Product";
+import Login from "./Pages/Login";
+// import Register from "./Pages/Register";
+
+// import Product from "./Pages/Product";
 
 // import ProductList from "./Pages/ProductList";
 
 const App = () => {
 	// return <Home />;
 	// return <ProductList />;
-	return <Product />;
+	// return <Product />;
+	// return <Register />;
+	return <Login />;
 };
 
 export default App;
